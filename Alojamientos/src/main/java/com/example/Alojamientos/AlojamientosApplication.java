503a6494daf3dc42a0c0f31af6592aed7bb21e59
DanielJNarvaezH <danielj.narvaezh@uqvirtual.edu.co>
2025-09-06 12:55:26 -0500

    Creacion del Proyecto

---
+++ Alojamientos/src/main/java/com/example/Alojamientos/AlojamientosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Alojamientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlojamientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlojamientosApplication.class, args);
+	}
+
+}
