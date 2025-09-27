d204a6e4879537584033c88e75c8488363cab733
DanielJNarvaezH <danielj.narvaezh@uqvirtual.edu.co>
2025-09-26 20:55:33 -0500

    Options en el AllowedMethods del cors

--- Alojamientos/src/main/java/com/example/Alojamientos/config/CorsConfig.java
+++ Alojamientos/src/main/java/com/example/Alojamientos/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/alojamientos/api/**")
                 .allowedOrigins(allowedOrigins)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
