4009ed12014b5b810955266fff78973816d19e78
DanielJNarvaezH <danielj.narvaezh@uqvirtual.edu.co>
2025-10-25 22:31:01 -0500

    Configuración de entornos (environment.ts ,environment.prod.ts y environment.test.ts)
    Configuracion de variables de ambiente para diferentes entornos, además de configuración de los replacement en el angular.json

---
+++ alojamientosHosped/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://miappproduccion.com/api',
+  appName: 'AlojamientosHosped (Producción)'
+};
---
+++ alojamientosHosped/src/environments/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://testing.miapi.com/api',
+  appName: 'AlojamientosHosped (Testing)'
+};
