9066c7e08fd305211d08126fd5e6766088ad2199
DanielJNarvaezH <danielj.narvaezh@uqvirtual.edu.co>
2025-10-27 11:13:59 -0500

    Directorio de Components se movio a dentro del directorio app,  y se creo primer atomo button en la ruta adecuada

--- alojamientosHosped/src/app/app-module.ts
+++ alojamientosHosped/src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Button } from './app/components/ad/atoms/button/button';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Button
   ],
   imports: [
     BrowserModule,
