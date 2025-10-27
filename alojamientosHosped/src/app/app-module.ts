77fc54268b92f29c62666874ae58e901a7eab16f
DanielJNarvaezH <danielj.narvaezh@uqvirtual.edu.co>
2025-10-27 11:16:39 -0500

    se crearon 2 atomos mas, label e input

--- alojamientosHosped/src/app/app-module.ts
+++ alojamientosHosped/src/app/app-module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Button } from './app/components/ad/atoms/button/button';
+import { Label } from './components/ad/atoms/label/label';
+import { Input } from './components/ad/atoms/input/input';
 
 @NgModule({
   declarations: [
     App,
-    Button
+    Button,
+    Label,
+    Input
   ],
   imports: [
     BrowserModule,
