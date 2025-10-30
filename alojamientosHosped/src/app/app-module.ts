470cb39fe7bc5ca340767393138ef495f53613e3
Juan <[200~juand.garciaa@uqvirtual.edu.co>
2025-10-30 10:43:14 -0500

    Correciones para funcionamiento de angular

--- alojamientosHosped/src/app/app-module.ts
+++ alojamientosHosped/src/app/app-module.ts
@@ -1,26 +1,28 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { Button } from './app/components/ad/atoms/button/button';
-import { Label } from './components/ad/atoms/label/label';
-import { Input } from './components/ad/atoms/input/input';
+
+// 🔹 Si aún no has creado estos componentes, comenta o elimina estas líneas temporalmente
+// import { Button } from './components/ad/atoms/button/button';
+// import { Label } from './components/ad/atoms/label/label';
+// import { Input } from './components/ad/atoms/input/input';
 
 @NgModule({
   declarations: [
     App,
-    Button,
-    Label,
-    Input
+    // Button,
+    // Label,
+    // Input
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([]) // Necesario para que funcione <router-outlet>
   ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
+  providers: [],
   bootstrap: [App]
 })
 export class AppModule { }
