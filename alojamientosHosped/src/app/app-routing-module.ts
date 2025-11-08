52339b976fd8e1231492d062f24fa58cb3dc4139
DanielJNarvaezH <danielj.narvaezh@uqvirtual.edu.co>
2025-11-08 11:12:41 -0500

    update en el app routing module

--- alojamientosHosped/src/app/app-routing-module.ts
+++ alojamientosHosped/src/app/app-routing-module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login/login'; // ✅ ruta corregida
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // redirige a login
+  { path: 'login', component: LoginPageComponent }      // carga tu página de login
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
